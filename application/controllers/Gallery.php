56f7c5989315d29c5fa14567f2b52da35350be33
chaos <chaos@142.232.52.63>
2015-09-18 16:34:04 -0700

    Tutorial basic03 (and Lab02) completed

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -21,6 +21,29 @@ class Gallery extends Application {
 	public function index()
 	{
             //$this->load->view('gallery');
+
+            // get all images from model
+            $pix = $this->images->all();
+            
+            // build array of formatted cells
+            foreach($pix as $picture) {
+                $cells[] = $this->parser->parse('_cell', (array)$picture, true);
+            }
+            
+            // prime the table class
+            $this->load->library('table');
+            $parms = array(
+                'table_open' => '<table class="gallery">',
+                'cell_start' => '<td class="oneimage">',
+                'cell_alt_start' => '<td class="oneimage">'
+            );
+            
+            $this->table->set_template($parms);
+            
+            // generate the table
+            $rows = $this->table->make_columns($cells, 3);
+            $this->data['thetable'] = $this->table->generate($rows);
+
             $this->data['pagebody'] = 'gallery';
             $this->render();
 	}
---
+++ application/views/_cell.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+
+<a href="./data/{filename}" data-lightbox="gallery"
+   data-title="{title} ... Posted {uploaded} by {uploader}, in {category}">
+    <img src="./data/thumb/{filename}"/>
+</a>
